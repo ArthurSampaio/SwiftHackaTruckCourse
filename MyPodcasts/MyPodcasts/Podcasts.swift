07297cbb00fa50580ccefa1451cb53961ea317ff
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2017-03-16 17:47:54 -0300

    3 app

---
+++ MyPodcasts/MyPodcasts/Podcasts.swift
@@ -0,0 +1,9 @@
+//
+//  Podcasts.swift
+//  MyPodcasts
+//
+//  Created by Student on 3/16/17.
+//  Copyright © 2017 Arthur Sampaio. All rights reserved.
+//
+
+import Foundation
---
+++ MyPodcasts/MyPodcasts/PodcastsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PodcastsTableViewCell.swift
+//  MyPodcasts
+//
+//  Created by Student on 3/16/17.
+//  Copyright © 2017 Arthur Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class PodcastsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
